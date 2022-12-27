13e41d6ffb529f33a355e3b7027f969bb2d4db85
saransh kumar <121025010+Saransh-tech@users.noreply.github.com>
2022-12-27 16:34:43 +0530

    Add files via upload

---
+++ infinte_circle.py
@@ -0,0 +1,24 @@
+from turtle import *
+import colorsys
+bgcolor("black")
+tracer(100)
+def draw():
+    h=0
+    n=40
+    for i in range(2,2900):
+        c=colorsys.hsv_to_rgb(h,1,1)
+        h +=1/n
+        up()
+        goto(0,0)
+        down()
+        color(c)
+        pensize(7)
+        fd(7)
+        rt(1)
+        lt(19)
+        circle(130)
+
+draw ()
+done()
+
+
